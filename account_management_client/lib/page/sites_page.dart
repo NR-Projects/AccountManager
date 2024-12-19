f73e80cbee6630622a02550078ba94347db5892d
Null-Root <98098486+Null-Root@users.noreply.github.com>
2024-12-19 17:00:10 +0800

    Revamp sites page

--- account_management_client/lib/page/sites_page.dart
+++ account_management_client/lib/page/sites_page.dart
@@ -17,207 +17,174 @@ class _SitesPageState extends State<SitesPage> {
   @override
   void initState() {
     super.initState();
-    initializeData();
+    _loadSites();
   }
 
-  Future<void> initializeData() async {
+  Future<void> _loadSites() async {
     final fetchedSites = await widget.siteService.getAllSites();
-    //print(fetchedSites[0].id);
     setState(() {
       sites = fetchedSites;
     });
   }
 
-  // Add a new Site object
-  Future<void> addSite(String name, String link) async {
-    await widget.siteService
-        .addNewSite(Site(id: UniqueKey().toString(), name: name, link: link));
-    await initializeData();
+  void _showSiteDetails(Site site) async {
+    showDialog(
+      context: context,
+      builder: (_) => AlertDialog(
+        title: Text(site.name),
+        content: Text('Link: ${site.link}\n\nAdditional details...'),
+        actions: <Widget>[
+          TextButton(
+            onPressed: () {
+              Navigator.pop(context);
+            },
+            child: const Text('Close'),
+          ),
+        ],
+      ),
+    );
   }
 
-  // Update an existing Site object
-  Future<void> updateSite(int index, String name, String link) async {
-    await widget.siteService
-        .updateExistingSite(Site(id: sites[index].id, name: name, link: link));
-    await initializeData();
+  void _editSite(Site site) {
+    TextEditingController nameController = TextEditingController(text: site.name);
+    TextEditingController linkController = TextEditingController(text: site.link);
+
+    showDialog(
+      context: context,
+      builder: (_) => AlertDialog(
+        title: const Text('Edit Site'),
+        content: Column(
+          mainAxisSize: MainAxisSize.min,
+          children: [
+            TextField(
+              controller: nameController,
+              decoration: const InputDecoration(labelText: 'Site Name'),
+            ),
+            TextField(
+              controller: linkController,
+              decoration: const InputDecoration(labelText: 'Site Link'),
+            ),
+          ],
+        ),
+        actions: <Widget>[
+          TextButton(
+            onPressed: () {
+              Navigator.pop(context);
+            },
+            child: const Text('Cancel'),
+          ),
+          TextButton(
+            onPressed: () async {
+              final updatedSite = Site(
+                id: site.id,
+                name: nameController.text,
+                link: linkController.text,
+              );
+              await widget.siteService.updateExistingSite(updatedSite);
+              _loadSites();
+              Navigator.pop(context);
+            },
+            child: const Text('Update'),
+          ),
+        ],
+      ),
+    );
   }
 
-  // Remove a site
-  Future<void> removeSite(int index) async {
-    await widget.siteService.deleteExistingSite(sites[index].id);
-    await initializeData();
+  void _deleteSite(String siteId) async {
+    await widget.siteService.deleteExistingSite(siteId);
+    _loadSites();
   }
 
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(
-        title: const Text("Sites"),
-        actions: [
-          IconButton(
-            icon: const Icon(Icons.add),
-            onPressed: () => _showAddOrUpdateSiteDialog(context),
-          ),
-        ],
+        title: const Text('Sites'),
+        centerTitle: true,
       ),
-      body: Padding(
-        padding: const EdgeInsets.all(8.0),
-        child: ListView(
-          children: [
-            Table(
-              border: TableBorder.all(color: Colors.grey),
-              columnWidths: const {
-                0: FlexColumnWidth(3), // Site name
-                1: FlexColumnWidth(2), // Actions
-              },
-              children: [
-                // Table header
-                const TableRow(
-                  decoration: BoxDecoration(color: Colors.blueGrey),
-                  children: [
-                    Padding(
-                      padding: EdgeInsets.all(8.0),
-                      child: Text(
-                        "Site Name",
-                        style: TextStyle(
-                            fontWeight: FontWeight.bold, color: Colors.white),
-                      ),
-                    ),
-                    Padding(
-                      padding: EdgeInsets.all(8.0),
-                      child: Text(
-                        "Actions",
-                        style: TextStyle(
-                            fontWeight: FontWeight.bold, color: Colors.white),
-                        textAlign: TextAlign.center,
-                      ),
-                    ),
-                  ],
-                ),
-                // Table rows with data
-                for (int index = 0; index < sites.length; index++)
-                  TableRow(
-                    children: [
-                      GestureDetector(
-                        onTap: () => _showSiteDetailsDialog(
-                          context,
-                          sites[index].name,
-                          sites[index].link,
-                        ),
-                        child: Padding(
-                          padding: const EdgeInsets.all(8.0),
-                          child: Text(
-                            sites[index].name,
-                            style: const TextStyle(
-                              fontSize: 16,
-                              fontWeight: FontWeight.w500,
-                              color: Colors.blue,
-                              decoration: TextDecoration.underline,
-                            ),
-                          ),
-                        ),
-                      ),
-                      Row(
-                        mainAxisAlignment: MainAxisAlignment.center,
-                        children: [
-                          IconButton(
-                            icon: const Icon(Icons.edit, color: Colors.orange),
-                            onPressed: () => _showAddOrUpdateSiteDialog(context,
-                                index: index),
-                          ),
-                          IconButton(
-                            icon: const Icon(Icons.delete, color: Colors.red),
-                            onPressed: () async => await removeSite(index),
-                          ),
-                        ],
-                      ),
-                    ],
+      body: RefreshIndicator(
+        onRefresh: _loadSites,
+        child: ListView.builder(
+          padding: const EdgeInsets.all(8),
+          itemCount: sites.length,
+          itemBuilder: (context, index) => siteItemCard(sites[index]),
+        ),
+      ),
+      floatingActionButton: FloatingActionButton(
+        onPressed: () {
+          TextEditingController nameController = TextEditingController();
+          TextEditingController linkController = TextEditingController();
+
+          showDialog(
+            context: context,
+            builder: (_) => AlertDialog(
+              title: const Text('Add New Site'),
+              content: Column(
+                mainAxisSize: MainAxisSize.min,
+                children: [
+                  TextField(
+                    controller: nameController,
+                    decoration: const InputDecoration(labelText: 'Site Name'),
                   ),
+                  TextField(
+                    controller: linkController,
+                    decoration: const InputDecoration(labelText: 'Site Link'),
+                  ),
+                ],
+              ),
+              actions: <Widget>[
+                TextButton(
+                  onPressed: () {
+                    Navigator.pop(context);
+                  },
+                  child: const Text('Cancel'),
+                ),
+                TextButton(
+                  onPressed: () async {
+                    final newSite = Site(
+                      id: UniqueKey().toString(),
+                      name: nameController.text,
+                      link: linkController.text,
+                    );
+                    await widget.siteService.addNewSite(newSite);
+                    _loadSites();
+                    Navigator.pop(context);
+                  },
+                  child: const Text('Add'),
+                ),
               ],
             ),
+          );
+        },
+        child: const Icon(Icons.add),
+      ),
+    );
+  }
+
+  Widget siteItemCard(Site site) {
+    return Card(
+      elevation: 3,
+      margin: const EdgeInsets.symmetric(vertical: 8, horizontal: 4),
+      child: ListTile(
+        title: Text(
+          site.name,
+          style: const TextStyle(fontWeight: FontWeight.bold),
+        ),
+        subtitle: Text('Link: ${site.link}'),
+        leading: const Icon(Icons.link, color: Colors.blue),
+        onTap: () => _showSiteDetails(site),
+        trailing: PopupMenuButton<String>(
+          onSelected: (value) {
+            if (value == 'Edit') _editSite(site);
+            if (value == 'Delete') _deleteSite(site.id);
+          },
+          itemBuilder: (context) => [
+            const PopupMenuItem(value: 'Edit', child: Text('Edit')),
+            const PopupMenuItem(value: 'Delete', child: Text('Delete')),
           ],
         ),
       ),
-    );
-  }
-
-  // Show a dialog to add or update a site
-  void _showAddOrUpdateSiteDialog(BuildContext context, {int? index}) {
-    final isEditing = index != null;
-    final TextEditingController nameController = TextEditingController(
-      text: isEditing ? sites[index].name : "",
-    );
-    final TextEditingController linkController = TextEditingController(
-      text: isEditing ? sites[index].link : "",
-    );
-
-    showDialog(
-      context: context,
-      builder: (BuildContext context) {
-        return AlertDialog(
-          title: Text(isEditing ? "Update Site" : "Add Site"),
-          content: Column(
-            mainAxisSize: MainAxisSize.min,
-            children: [
-              TextField(
-                controller: nameController,
-                decoration: const InputDecoration(labelText: "Site Name"),
-              ),
-              TextField(
-                controller: linkController,
-                decoration: const InputDecoration(labelText: "Site Link"),
-              ),
-            ],
-          ),
-          actions: [
-            TextButton(
-              child: const Text("Cancel"),
-              onPressed: () => Navigator.of(context).pop(),
-            ),
-            TextButton(
-              child: Text(isEditing ? "Update" : "Add"),
-              onPressed: () {
-                final name = nameController.text.trim();
-                final link = linkController.text.trim();
-                if (name.isNotEmpty) {
-                  if (isEditing) {
-                    updateSite(index, name, link);
-                  } else {
-                    addSite(name, link);
-                  }
-                  Navigator.of(context).pop();
-                }
-              },
-            ),
-          ],
-        );
-      },
-    );
-  }
-
-  // Show a dialog with site details
-  void _showSiteDetailsDialog(
-      BuildContext context, String? name, String? link) {
-    showDialog(
-      context: context,
-      builder: (BuildContext context) {
-        return AlertDialog(
-          title: Text(name ?? "Site Details"),
-          content: SelectableText(
-            link ?? "No link available",
-            style: const TextStyle(
-              color: Colors.blue,
-              decoration: TextDecoration.underline,
-            ),
-          ),
-          actions: [
-            TextButton(
-              child: const Text("Close"),
-              onPressed: () => Navigator.of(context).pop(),
-            ),
-          ],
-        );
-      },
     );
   }
 }
